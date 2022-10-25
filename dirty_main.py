41839a93da0f56d12b892f5ff77486e0d5330a9b
Mikhail <misha.shabalin@mail.ru>
2022-10-25 21:29:59 +0300

    Read

---
+++ dirty_main.py
@@ -0,0 +1,8 @@
+# Можно ещё грязнее)
+from application import *
+# from application.salary import *
+# from application.db.people import *
+
+if __name__ == '__main__':
+    calculate_salary()
+    get_employees()
